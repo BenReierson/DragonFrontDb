90b74befb6c7731a0c88f82fd351681f6ef8fd88
Ben R <ben.reierson@outlook.com>
2016-12-07 15:48:55 +0800

    v1.2.2

--- Info.cs
+++ Info.cs
@@ -12,7 +12,7 @@ namespace DragonFrontDb
         {
             get
             {
-                return new Version(1,2,1);
+                return new Version(1,2,2);
             }
         }
 
