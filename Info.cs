16062a49a2e4607622daa78636c82fdabc09c373
Ben Reierson <BenReierson@users.noreply.github.com>
2016-12-10 11:58:04 +1100

    Update Info.cs

--- Info.cs
+++ Info.cs
@@ -20,7 +20,7 @@ namespace DragonFrontDb
         {
             get
             {
-                return new Version(1, 00, 26412, 1);
+                return new Version(1, 1, 4, 1);
             }
         }
     }
